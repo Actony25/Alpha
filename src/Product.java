e81103e025e32050868e8e2128e5260b65c2f6df
Anthony Cangelosi <actony25@hotmail.com>
2018-09-29 19:48:57 -0400

    Commit

--- src/Product.java
+++ src/Product.java
@@ -2,12 +2,44 @@ import java.util.Date;
 
 public abstract class Product implements Item {
 
-  int serialNumber;
-  String manufacturer;
-  Date manufacuredOn;
-  String name;
+  private int serialNumber;
+  private String manufacturer;
+  private Date manufacuredOn;
+  private String name;
 
-  int currentProductionNumber;
+  private int currentProductionNumber;
 
-  
+  public Product(String name) {
+    this.name = name;
+    serialNumber = currentProductionNumber++;
+    manufacuredOn.getTime();
+  }
+
+  public void setProductionNumber(int pn) {
+    currentProductionNumber = pn;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Date getManufacturerDate() {
+    return manufacuredOn;
+  }
+
+  public int getSerialNumber() {
+    return serialNumber;
+  }
+
+  public String toString() {
+    return
+          "Manufacturer  : " + manufacturer + "\n"
+        + "Serial Number : " + serialNumber + "\n"
+        + "Date          : " + manufacuredOn + "\n"
+        + "Name          : " + name + "\n";
+  }
 }
