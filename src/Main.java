7785862abc38547379438c791546591cb5842b21
Anthony Cangelosi <actony25@hotmail.com>
2018-12-05 15:21:55 -0500

    Completed step 22 working on 23

--- src/Main.java
+++ src/Main.java
@@ -14,58 +14,110 @@ public class Main {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
-    int menu = 1; //Menu for allowing user to continue using the program
-    int answer; //Variable for storing users choice
+    boolean programState = true; //User choice for allowing user to continue using the program
+    int mainMenuChoice = 0; //Variable for storing users choice
+    int productMenuChoice = 0; //Variable for storing users product choice
+    ArrayList<Product> products = new ArrayList<>(); //ArrayList for the collection of products
 
     System.out.println("Welcome to the Oracle Production Line\n");
 
-    while (menu == 1) {
-      System.out.println("What would you like to do today? (Enter a number)");
-      System.out.println("1. Add a product");
-      System.out.println("2. Display current products");
-      System.out.println("3. Exit the system");
+    do { //Loop for continued program state. Will continue if true
+      do { //Loop for menu choice. Will continue if answer is not between 1 and 3
+        System.out.println("What would you like to do today? (Enter a number)");
+        System.out.println("1. Add a new product");
+        System.out.println("2. Display current products");
+        System.out.println("3. Exit the system");
 
-      try {
-        answer = scan.nextInt();
-      } catch (InputMismatchException ime) {
-        System.out.println("Incorrect input");
-        System.out.println("Please enter an option number");
-        continue;
+        try {
+          mainMenuChoice = scan.nextInt();
+        } catch (InputMismatchException ime) {
+          System.out.println("Incorrect input");
+          System.out.println("Please enter an option number\n");
+          scan.nextLine(); //Empties the scanner
+        }
+      } while (mainMenuChoice < 1
+          || mainMenuChoice > 3); //End menuChoice loop. Will end if answer is between 1 and 3
+
+      switch (mainMenuChoice) {
+        case 1: //Start case 1: Add new product
+          do {
+            System.out.println("Which product would you like to add? (Enter a number)");
+            System.out.println("1. Audio Player");
+            System.out.println("2. Movie Player");
+            System.out.println("3. Return to previous menu");
+
+            try {
+              productMenuChoice = scan.nextInt();
+            } catch (InputMismatchException ime) {
+              System.out.println("Wrong input");
+              System.out.println("Please enter an option number\n");
+              scan.nextLine(); //Empties the scanner
+            }
+          } while (productMenuChoice < 1
+              || productMenuChoice
+              > 3); //End productMenuChoice loop. Will end if answer is between 1 and 3
+
+          switch (productMenuChoice) {
+            case 1: //Start case 1: Audio Player
+              AudioPlayer a1 = new AudioPlayer("iPod Mini", "MP3");
+              products.add(a1);
+              System.out.println("Successfully added audio player");
+              scan.nextLine(); //Empties the scanner
+              break; //End case 1: Audio Player
+            case 2: //Start case 2: Movie Player
+              MoviePlayer m1 = new MoviePlayer("DBPOWER MK101",
+                  new Screen(" 720x480", 40, 22), MonitorType.LCD);
+              products.add(m1);
+              System.out.println("Successfully added movie player");
+              scan.nextLine(); //Empties the scanner
+              break; //End case 2: Movie Player
+            default: //Start default: Return to previous menu
+              break; //End default: Return to previous menu
+          }
+          break; //End case 1: Add a new product
+
+        case 2: //Start case 2: Display current products
+          if (products.isEmpty()) {
+            System.out.println("There are no products currently in the system\n");
+          } else {
+            Collections.sort(products);
+            print(products);
+          }
+          break; //End case 2: Display current products
+
+        case 3: //Start case 3: Exit the system
+          System.out.println("Have a nice day. Goodbye");
+          programState = false;
+          break; //End case 3: Exit the system
+
+        default: //Start default: Exit the system
+          programState = false;
+          break; //End default: Exit the system
       }
 
-      if (answer == 3) {
-        System.out.println("Have a nice day. Goodbye");
-        break;
-      }
-      scan.nextLine();
-
-
-
-      /*
-      // Write one line of code to create an ArrayList of products
-      ArrayList<Product> products;
-      // Write one line of code to call testCollection and assign the result to the ArrayList
-      products = testCollection();
-      // Write one line of code to sort the ArrayList
-      Collections.sort(products);
-      // Call the print method on the ArrayList
-      print(products);
-      */
-
-      /*
-      //Test the EmployeeInfo class
-      EmployeeInfo em = new EmployeeInfo();
-      System.out.println(em.toString());
-      //Test the ProcessFile class
-      ProcessFile pf = new ProcessFile();
-      try {
-        pf.WriteFile(products);
-      } catch (Exception ex) {
-        System.out.println("Problem writing to file");
-      }
-      */
-    }
+    } while (programState); //End programState loop. Program will end if false
   }
+    /*
+    // Write one line of code to create an ArrayList of products
+    ArrayList<Product> products;
+    // Write one line of code to call testCollection and assign the result to the ArrayList
+    products = testCollection();
+    // Write one line of code to sort the ArrayList
+    Collections.sort(products);
+    // Call the print method on the ArrayList
+    print(products);
+
+    //Test the EmployeeInfo class
+    EmployeeInfo em = new EmployeeInfo();
+    System.out.println(em.toString());
+
+    //Test the ProcessFile class
+    ProcessFile pf = new ProcessFile();
+    try {
+      pf.WriteFile(products);
+    } catch (Exception ex) {
+      System.out.println("Problem writing to file");
+    }
 
   // Step 15
   // Complete the header for the testCollection method here
@@ -88,7 +140,7 @@ public class Main {
     products.add(m1);
     products.add(m2);
     return products;
-  }
+  }*/
 
   // Step 16
   // Create print method here
